6425b3087cd6a59fa6ba4b8e3d350114b4763298
IT24100017 <it24100017@my.sliit.lk>
2024-10-06 23:00:11 +0530

    IT24100017Lab7Q2A

---
+++ Q2/IT24100017Lab7Q2A.java
@@ -0,0 +1,9 @@
+public class IT24100017Lab7Q2A {
+	public static void main(String[] args) {
+		System.out.println("$ $ $ $ $") ;
+		System.out.println("$ $ $ $ $") ;
+		System.out.println("$ $ $ $ $") ;
+		System.out.println("$ $ $ $ $") ;
+
+	}
+}
